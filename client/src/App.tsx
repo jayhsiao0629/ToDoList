75c7c1186167084c44990ffaa371b85b25115954
jayhsiao <shjay629@gmail.com>
2025-04-26 15:03:44 +0800

    add todolist

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, HStack } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar />
         <Container>
           <TodoForm />
+          <TodoList />
         </Container>
     </HStack>
   );
